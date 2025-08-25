9617bdc6e83bf702729479d3154bdd76b9a88d5f
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-08-25 10:34:00 -0300

    Aula 25

---
+++ aula25/dicionarios/aluno.py
@@ -0,0 +1,12 @@
+aluno = {
+    'nome': 'Jonas',
+    'matricula': '123',
+    'idade':'18',
+    'altura': 1.8
+}
+
+print(aluno)
+print(aluno['nome'])
+aluno['nome'] = 'Jonas Silva'
+aluno['notas'] = [3,8,10]
+print(aluno)
---
+++ aula25/tuplas/tuplas.py
@@ -0,0 +1,12 @@
+a = (1,2,3)
+b = (4,5,6)
+c = a + b
+
+coord = (3,5)
+x, y = coord
+print(f'x = {x}, y = {y}')
+j = ('valor',)
+print(type(j))
+print(b[-1])
+d = a * 3
+print(d)
